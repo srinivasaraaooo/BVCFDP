00f159b2f71c96499218ca2c38cd17a6976f6c24
srinivasaraaooo <53079181+srinivasaraaooo@users.noreply.github.com>
2025-12-17 11:06:32 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("bvc college app")
+st.write("Welcome to your bvc  college application")   
+st.write("This app is designed to provide information about bvc college")
+st.write    
+
+
+
+
+
